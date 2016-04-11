641ef4ecadb7ef072603cd2e4a103ba0eeba0651
Zeal Dalal <zeal_dalal@yahoo.co.in>
2016-04-11 19:02:31 -0400

    seeder

---
+++ database/seeds/TweetsSeeder.php
@@ -0,0 +1,24 @@
+<?php
+use Illuminate\Database\Seeder;
+
+class UsersSeederTable extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        $limit = 10;
+
+        for($i=0;$i<$limit;$i++) {
+            DB::table('users')->insert([
+                'user_id'=>$faker->numberBetween($min = 1, $max = 10),
+                'tweet' => $faker->text($maxNbChars = 50),
+            ]);
+        }
+    }
+}
