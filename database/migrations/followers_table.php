9ac53991a2e5a737a4484139f5f795b4f0735274
Zeal Dalal <zeal_dalal@yahoo.co.in>
2016-04-11 19:19:46 -0400

    seeder

---
+++ database/migrations/followers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('_follower_user_id')->unsigned();
+            $table->foreign('follower_user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('followers');
+    }
+}
